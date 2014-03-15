638fa39711e3783a19dfcee494a63596c7c3831e
John Louis Walker <injyuw@gmail.com>
2014-03-15 13:30:59 -0400

    +core: (-)init-workspace, (-)project-controller
    Add two functions for setting up a gephi visualization.

--- src/gephi_clj/core.clj
+++ src/gephi_clj/core.clj
@@ -1,6 +1,24 @@
-(ns gephi-clj.core)
+(ns gephi-clj.core
+  (import [org.gephi.io.importer.api Container ImportController]
+          [org.gephi.project.api ProjectController Workspace]
+          [org.openide.util Lookup])
+  (require [clojure.reflect :as r]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn- project-controller []
+  (.lookup (Lookup/getDefault) ProjectController))
+
+(defn- init-workspace
+  ([]
+     (init-workspace (project-controller)))
+  ([project]
+     (.newProject project)
+     (.getCurrentWorkspace project)))
+
+;; (init-workspace)
+
+;; (let [f (clojure.java.io/file "resources/polblogs.gml")
+;;       importController (.lookup
+;;                         (Lookup/getDefault)
+;;                         ImportController)
+;;       container (-> importController (.importFile f))]
+;;   container)
