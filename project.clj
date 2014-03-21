637ff81d743399a804d029017a80fda219b3b23d
John Louis Walker <injyuw@gmail.com>
2014-03-20 21:54:51 -0400

    Add potemkin dependency for exposing API

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :repositories {"gephi releases" "http://nexus.gephi.org/nexus/content/repositories/releases/"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.gephi/gephi-toolkit "0.8.2"]])
+                 [org.gephi/gephi-toolkit "0.8.2"]
+                 [potemkin "0.3.4"]])
