4d7be18bbcbe7a429151d1a012ecda4919faa97e
John Louis Walker <injyuw@gmail.com>
2014-03-20 21:52:45 -0400

    +core:process-graph!,export!,visualize!. More
    Also add:
    
    get-controller
    get-workspace
    ->container
    
    Remove:
    
    init-workspace
    project-controller

--- src/gephi_clj/core.clj
+++ src/gephi_clj/core.clj
@@ -1,24 +1,63 @@
 (ns gephi-clj.core
-  (import [org.gephi.io.importer.api Container ImportController]
+  (import [org.gephi.io.importer.api Container ImportController ContainerFactory]
+          [org.gephi.io.exporter.api ExportController]
+          [org.gephi.io.exporter.spi GraphExporter]
           [org.gephi.project.api ProjectController Workspace]
-          [org.openide.util Lookup])
-  (require [clojure.reflect :as r]))
+          [org.openide.util Lookup]
+          [org.gephi.io.processor.plugin DefaultProcessor AppendProcessor]))
 
-(defn- project-controller []
-  (.lookup (Lookup/getDefault) ProjectController))
+(defn get-controller [c]
+  (.lookup (Lookup/getDefault) c))
 
-(defn- init-workspace
+(defn ->container []
+  (-> (Lookup/getDefault)
+     (.lookup ContainerFactory)
+     .newContainer))
+
+(defn get-workspace
   ([]
-     (init-workspace (project-controller)))
+     (get-workspace (get-controller ProjectController)))
   ([project]
      (.newProject project)
      (.getCurrentWorkspace project)))
 
-;; (init-workspace)
+(defn export! [filename]
+  (.exportFile (get-controller ExportController) (clojure.java.io/file filename)))
 
-;; (let [f (clojure.java.io/file "resources/polblogs.gml")
-;;       importController (.lookup
-;;                         (Lookup/getDefault)
-;;                         ImportController)
-;;       container (-> importController (.importFile f))]
-;;   container)
+(defn process-graph!
+  ([graph]
+     (process-graph! graph
+                     (DefaultProcessor.)))
+  ([graph processor]
+     (process-graph! graph
+                     processor
+                     (->container)
+                     (get-controller ImportController)
+                     (get-workspace)))
+  ([graph processor container import-controller workspace]
+     (.generate graph (.getLoader container))
+     (.process import-controller container processor workspace)))
+
+(defn visualize!
+  ([item exportfn]
+     (let [r (visualize! item)]
+       (exportfn)
+       r))
+  ([item]
+     (let [workspace (get-workspace)
+           container (->container)
+           controller (get-controller ImportController)
+           {:keys [graph processor]
+            :or   {processor (DefaultProcessor.)}} item]
+       (process-graph! graph processor container controller workspace)
+       {:workspace workspace
+        :container container
+        :controller controller})))
+
+;; (visualize! 
+;;  {:graph (random-graph :number-of-nodes 6
+;;                        :wiring-probability 0.05)
+;;   :processor (DefaultProcessor.)}
+;;  #(export! "random-graph.png"))
+
+
